e56e2e6798d76fd1be1bb237dba233662bcbc014
BlexxeCode <102894148+BlexxeCode@users.noreply.github.com>
2023-04-11 03:40:09 -0400

    Update config.py

--- Website/config.py
+++ Website/config.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password@12'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'steam'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
